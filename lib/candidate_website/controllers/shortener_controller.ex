4d60110a938f6d377ceb802dfce438fc1fff0e7d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-29 12:52:35 -0500

    redirect not founds to jdems.us

--- lib/candidate_website/controllers/shortener_controller.ex
+++ lib/candidate_website/controllers/shortener_controller.ex
@@ -3,31 +3,10 @@ defmodule CandidateWebsite.ShortenerController do
 
   def index(conn = %{request_path: path}, _params) do
     path = String.downcase(path)
-    routes = CandidateWebsite.AirtableShortlinks.get_all()
-
-    tuple_or_nil =
-      routes
-      |> Enum.filter(&matches(&1, path))
-      |> List.first()
-
-    destination =
-      case tuple_or_nil do
-        nil -> "https://justicedemocrats.com"
-        {_, destination} -> destination
-      end
-
-    redirect(conn, external: https_prefix(destination))
+    redirect(conn, external: jdems_prefix(path))
   end
 
-  defp matches({regex, destination}, path) do
-    Regex.run(regex, path) != nil
-  end
-
-  defp https_prefix(url = "http" <> _rest) do
-    url
-  end
-
-  defp https_prefix(url) do
-    "https://" <> url
+  defp jdems_prefix(path) do
+    "https://jdems.us/#{path}"
   end
 end
