a9ee28a5af832f8844edd6aa3d6a188705e1edec
Chris Wetherell <chrisw@gmail.com>
2018-02-09 13:01:15 -0800

    Remove inspect

--- lib/main_website/controllers/page_controller.ex
+++ lib/main_website/controllers/page_controller.ex
@@ -2,7 +2,6 @@ defmodule MainWebsite.PageController do
   use MainWebsite, :controller
 
   def index(conn, _params) do
-    IO.inspect(Application.get_env(:main_website, :css_src, ""))
     assigns = Map.get(conn.assigns, :data, %{})
     render(conn, "index.html", Enum.into(assigns, []))
   end
