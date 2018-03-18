cf1af54cfa6bcf732084ac5741684a02d2c6ca78
Chris Wetherell <chrisw@gmail.com>
2018-03-18 01:22:48 -0700

    Use Cosmic for mission placeholder

--- lib/main_website/helpers/view_helpers.ex
+++ lib/main_website/helpers/view_helpers.ex
@@ -5,6 +5,7 @@ defmodule MainWebsite.ViewHelpers do
 
   use Phoenix.HTML
   import MainWebsite.Router.Helpers
+  import ShortMaps
 
   @doc """
   Is the given router action being viewed?
@@ -12,4 +13,9 @@ defmodule MainWebsite.ViewHelpers do
   def is_current_path(conn, action), do: page_path(conn, action) == conn.request_path
 
   def static_path(), do: Application.get_env(:main_website, :static_path, "")
+
+  def cosmic(path, slug \\ "project-taquito-2") do
+    ~m(content) = Cosmic.get(path, slug)
+    content
+  end
 end
